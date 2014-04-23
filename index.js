3ca77590977d7bb1ccacc10faa3aa756523da138
Richard Kraaijenhagen <richard@owlin.com>
2014-04-23 16:58:35 +0200

    Fuck jade

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-var express = require('express')
-    , stylus = require('stylus')
-    , nib = require('nib');
+var express = require('express'),
+  app = express(),
+  swig = require('swig'),
+  nib = require('nib'),
+  stylus = require('stylus'),
+  people;
 
-var app = express();
-
-var app = express()
-
-function compile(str, path) {
-    return stylus(str)
-    .set('filename', path)
-    .use(nib())
+function compile(str, path){
+    return stylus(str).set('filename',path).use(nib())
 }
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(stylus.middleware({ 
-    src: __dirname + '/public', 
-    compile: compile
-}))
-app.use(express.static(__dirname + '/public'))
+// This is where all the magic happens!
+app.engine('html', swig.renderFile);
 
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.use(stylus.middleware({
+    src : __dirname + '/public',
+    compile : compile
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+// Swig will cache templates for you, but you can disable
+// that and use Express's caching instead, if you like:
+app.set('view cache', false);
+// To disable Swig's cache, do the following:
+swig.setDefaults({ cache: false });
+// NOTE: You should always cache templates in a production environment.
+// Don't leave both of these to `false` in production!
 
 app.get('/', function (req, res) {
-    res.render('index',
-        { title : 'Home' }
-    )
-})
+  res.render('index', { /* template locals context */ });
+});
 
-app.listen(3000);
+app.listen(1337);
+console.log('Application Started on http://localhost:1337/')
 
